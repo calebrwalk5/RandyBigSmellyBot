efdc685f366ab52be499845834b48064844b9760
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2020-11-09 18:18:17 -0500

    Update start-bot-linux.sh

--- start-bot-linux.sh
+++ start-bot-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if test "`whoami`" != "root" ; then
-	echo "You must be logged in as root to build (for loopback mounting)"
+	echo "You must be logged in as root "
 	echo "Enter 'su' or 'sudo bash' to switch to root"
 	exit
 fi
