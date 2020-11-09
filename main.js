e76c2c8c449b9a24f5f80ec6a8eb486a298c6d74
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2020-11-08 20:20:26 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Built By: Hyperz#0001
+// Contribution By: calebrwalk5#6969
 
+// Declare variables
 const fs = require('fs');
 const Discord = require('discord.js');
 const ws = require('ws');
@@ -15,6 +17,7 @@ client.user.setPresence({ activity: { name: `For Randy...`, type: `WATCHING` }, 
 console.log(`${client.user.tag}, By Hyperz#0001 is now watching for Randy :D!`);
 });
 
+// Main function of bot
 client.on('message', message => {
   if(message.content.includes === 'hyperz')
   message.channel.send('Hyperz is fucking amazing, literally change my mind, oh wait, you cant because Hyperz programmed me into saying this regardless of what my bot brain actually thinks... POGGERS!')
@@ -34,14 +37,27 @@ client.on('message', message => {
 client.on('message', message => {
   if(message.content.includes === 'hyperz')
   message.channel.send('https://youtube.com/itzhyperz')
+  wehaterandy();
 });
 
 client.on('message', message => {
   if(message.content.includes === 'randy')
   message.channel.send('Randy laughed at Hyperz shitty life #fuckrandy!')
+  wehaterandy();
 });
 
+client.on('message', message => {
+  if(message.content.includes === 'randy')
+  wehaterandy();
+});
+
+// Caleb's functions
+function wehaterandy() {
+  client.channels.get('747930604624150532').send('we all hate Randy');
+  client.users.get("Hyperz#0001").send("we hate randy message sent");
+}
 // Logs the bot in
 client.login('your-bot-token-goes-here');
 
 // Built By: Hyperz#0001
+// And calebrwalk5#6969
